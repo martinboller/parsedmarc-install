0f4e770a966fa413d9a30bb3ba4928ca5685c9e8
Martin <martin@riptorn.bollers.dk>
2021-01-12 19:03:45 +0100

    Tested w. Parsedmarc 6.12.0

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Automated install of parsedmarc with Elasticsearch on Debian
 
 For information about the great parsedmarc project see: https://github.com/domainaware/parsedmarc
-Tested with parsedmarc v. 6.6.1
+Tested with parsedmarc v. 6.12.0 and Elastic 7.8
--- parsedmarc-install.sh
+++ parsedmarc-install.sh
@@ -5,8 +5,8 @@
 # Author:       Martin Boller                                       #
 #                                                                   #
 # Email:        martin                                              #
-# Last Update:  2019-09-27                                          #
-# Version:      1.42                                                #
+# Last Update:  2021-01-12                                          #
+# Version:      1.45                                                #
 #                                                                   #
 # Changes:      Initial Version (1.00)                              #
 #               Added logging dir (1.10)                            #
@@ -14,6 +14,7 @@
 #               Installing version 4.6.2 (1.30)                     #
 #               Installing parsedmarc 6.5.3 (1.40)                  #
 #               Tested with parsedmarc=6.7.2 (1.42)                 #
+#               Tested with parsedmarc=6.12.0 (1.45)                #
 #                                                                   #
 # Description:  Installs parsedmarc as a service (paas :)           #
 # Info:         https://domainaware.github.io/parsedmarc/           #
